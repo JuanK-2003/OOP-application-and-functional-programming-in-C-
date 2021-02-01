b61923487ba48d60559d6141d13bd06e1a0ddf75
Juan Carlos Neíl Palacios Escobar <67353415+JuanK-2003@users.noreply.github.com>
2021-02-01 17:57:12 -0600

    Add files via upload

---
+++ person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Person
+    {
+        string name;
+        string direction;
+        DateTime dateNaci;
+
+        public string Name { get => name; set => name = value; }
+        public string Direction { get => direction; set => direction = value; }
+        public DateTime DateNaci { get => dateNaci; set => dateNaci = value; }
+
+        public int calcAge()
+        {
+            int age = DateTime.Today.AddTicks(-DateNaci.Ticks).Year - 1;
+            return age;
+        }
+
+        public String normName()
+        {
+            List<String> normalized = Name.Split(' ').ToList();
+            normalized.ForEach(word => word.ToLower());
+            Name = "";
+            foreach(var word in normalized)
+            {
+                var n = char.ToUpper(word[0]) + word.Substring(1);
+                name += " " + n;
+            }
+            return Name;
+        }
+
+    }
+}
